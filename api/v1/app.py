c37cfd791aff608e892b1dbbde522dfecb5d8aa7
Felipe <felipe_londono1@hotmail.com>
2020-04-28 21:04:07 +0000

    [Fix]: corrected error message

--- api/v1/app.py
+++ api/v1/app.py
@@ -15,11 +15,9 @@ def close(exception):
     """ close the storage """
     storage.close()
 
-
 @app.errorhandler(404)
-def page_not_found(e):
-    """ error 404 handler """
-    return jsonify({"error": "Not found"})
+def resource_not_found(e):
+    return jsonify(error=str(e)), 404
 
 if __name__ == "__main__":
     API_HOST = getenv('HBNB_API_HOST')
